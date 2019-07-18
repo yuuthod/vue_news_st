5f8a056faa777b16b944ed981440e0fcb2d00c20
yurim Lee <yuuthod@we-gokorea.com>
2019-07-18 20:36:35 +0900

    axios를 이용한 api호출

--- vue-news/src/Views/NewsView.vue
+++ vue-news/src/Views/NewsView.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>news</div>
+  <div>
+    <h1>News</h1>
+    <ul>
+      <li v-for="user in users">{{user}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data() {
+    return {
+      users: []
+    }
+  },
+  created() {
+    // new Promise() -> axios
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(response => this.users = response.data)
+      .catch(error => console.log(error))
+    // 화살표 함수 쓰지 않았을 경우
+    // var vm = this 를 통해 바인딩을 시켜줘야한다.
+  }
 }
 </script>
 
